512fb032e77df1c9792d20cdc7274f8f8d9a2650
Lemon2311 <marcotighiliu@yahoo.com>
2023-12-18 20:56:07 +0100

    working on main functionality

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ from human_detection_model import predict_image
 
 
 capture_photo("capture")
-crop_and_save_images("capture", "cropped", 1400)
-predict_image("cropped/image.jpg")
+crop_and_save_images("capture", "cropped", 1400)#crop size needs to be adjusted
+print(predict_image("cropped/image.jpg"))
