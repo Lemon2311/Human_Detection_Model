d2d83d6d9a510370982a6df259b59e7274d1ad1b
Lemon2311 <marcotighiliu@yahoo.com>
2023-12-21 23:39:38 +0100

    fixed typo

--- main.py
+++ main.py
@@ -11,4 +11,4 @@ crop_and_save_images("capture", "cropped", 1400)#crop size needs to be adjusted
 
 new_image_class = predict_image(loaded_model, 'capture/image.jpg')
 # Print the predicted class
-print("Predicted class:", "Person" if new_image_class[0] == 0 else "Dog")
+print("Predicted class:", "Person" if new_image_class[0] == 0 else "Person")
